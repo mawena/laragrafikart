dfb35283b28e83fffabd229117cc9264ea574c67
mawena <gamligocharles@gmail.com>
2023-05-05 10:02:55 +0000

    10-Debugar et IDE Helper

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,6 +9,8 @@
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
     <link rel="stylesheet" href="/css/bootstrap-sketchy.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap">
     <style>
         @layer demo {
             button {
@@ -25,11 +27,13 @@
 @endphp
 
 <body>
-    @include('Parts.nav')
+    @include('parts.nav')
 
     <div class="container">
         @yield('content')
     </div>
+
+    @include('parts.footer')
 </body>
 
 </html>
---
+++ resources/views/parts/footer.blade.php
@@ -0,0 +1,27 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+
+<footer class="text-center text-lg-start bg-primary">
+    <div class="container d-flex justify-content-center py-5">
+        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+            <i class="fab fa-facebook-f"></i>
+        </button>
+        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+            <i class="fab fa-youtube"></i>
+        </button>
+        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+            <i class="fab fa-instagram"></i>
+        </button>
+        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+            <i class="fab fa-twitter"></i>
+        </button>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2023 Copyright:
+        <a class="text-white" href="{{ route('blog.index') }}">LaraGrafikart</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+
+<!-- End of .container -->
